e0d14cb1d8bfe8db42176a4f05cc678a0bee0df4
jeff <jeffk713@gmail.com>
2021-11-01 13:22:07 -0700

    implement mocha chai in middleTest.js and tailTest.js files

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,11 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]), [6, 7]);
+describe('#middle', () => {
+  it('should return one element from an array with odd numbers of element ', () => {
+    assert(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
+  });
+  it('should return two element from an array with even numbers of element ', () => {
+    assert(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]), [6, 7]);
+  });
+});
