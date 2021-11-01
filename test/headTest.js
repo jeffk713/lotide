e2072468ccd1225fed45692e59beb80308070985
jeff <jeffk713@gmail.com>
2021-11-01 13:10:22 -0700

    implemet mocha chai in headTest.js file

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,14 @@
+const assert = require('chai').assert;
 const head = require('../head');
-const assertEqual = require('../assertEqual');
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
-console.log(head([]));
+describe('#head', () => {
+  it('returns 5 for [5, 6, 7]', () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+  it("returns '5' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
+  it('returns undefined for []', () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});
