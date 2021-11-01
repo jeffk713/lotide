225ded85786cdfa11ff794df9d51804b33504e2f
jeff <jeffk713@gmail.com>
2021-11-01 12:47:51 -0700

    implement middleTest.js

--- middle.js
+++ middle.js
@@ -1,20 +1,3 @@
-const eqArrays = (arr1, arr2) => {
-  let result = true;
-  arr1.forEach((num, i) => {
-    if (num !== arr2[i]) return (result = false);
-  });
-
-  return result;
-};
-
-const assertArraysEqual = (arr1, arr2) => {
-  const assertion = eqArrays(arr1, arr2);
-
-  if (assertion) return console.log('✅✅✅Assertion passes');
-
-  if (!assertion) return console.log('💩💩💩Assertion fails');
-};
-
 const middle = arr => {
   let arrMiddle = [];
   if (arr.length < 3) return arrMiddle;
@@ -32,5 +15,4 @@ const middle = arr => {
   return arrMiddle;
 };
 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]), [6, 7]);
+module.exports = middle;
